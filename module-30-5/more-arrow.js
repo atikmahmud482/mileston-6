7155e1e91da5dffd027fa505987fac86de2f4cda
atikmahmud482 <atikmahmud482@gmail.com>
2024-12-26 00:26:02 +0600

    module-30-5

---
+++ module-30-5/more-arrow.js
@@ -0,0 +1,27 @@
+const difference = (x, y) => x - y;
+const multiple = (first, second) => first * second;
+const getAge = (person) => person.age;
+const student = { name: 'John', age: 30 };
+const age = getAge(student);
+console.log(age);
+
+const getThird = (numbers) => numbers[2];
+const third = getThird([1, 2, 3, 4, 5]);
+console.log(third);
+
+const doubleIt = (num) => num * 2;
+const result = doubleIt(5);
+console.log(result);
+
+const getPI = () => Math.PI;
+const pi = getPI();
+console.log(pi);
+
+const doMath = (x, y) => {
+    const sum = x + y;
+    const diff = x - y;
+    const result = sum * diff;
+    return result;
+}
+const result2 = doMath(7, 5);
+console.log(result2);
