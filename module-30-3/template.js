e0a775d8ce4da6141519d63048389d5db307f5c1
atikmahmud482 <atikmahmud482@gmail.com>
2024-12-25 23:58:22 +0600

    module-30-3

---
+++ module-30-3/template.js
@@ -0,0 +1,35 @@
+const first = 'Justin';
+const last = 'TimberLake';
+const greet = 'potash';
+
+const name = first + ' ' + last + ' ' + greet;
+console.log(name);
+
+const a = 10;
+const b = 20;
+const result = a + b;
+const result3 = `the sum of a and b is ${result}`;
+console.log(result3);
+
+// no1: \n for new line
+const multiLine1 = 'I love you\n' +
+    'I miss you\n' +
+    'I need you';
+// console.log(multiLine1);
+// no2: \t for tab space
+const multiLine2 = 'I love you\n'
+    + 'I miss you\n'
+    + 'I need you';
+// console.log(multiLine2);
+// no3: `` for template string
+
+// no4: ${} for dynamic value
+const multiLine3 = `I love you
+I miss you
+I need you`;
+// console.log(multiLine3);
+// no5: multiple line string
+const multiLine = `I love you
+I miss you
+I need you`;
+// console.log(multiLine);
