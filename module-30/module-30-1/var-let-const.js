58125998de3a45fbdfbbd116d4e851da3755e092
atikmahmud482 <atikmahmud482@gmail.com>
2024-12-25 17:54:05 +0600

    first commit

---
+++ module-30/module-30-1/var-let-const.js
@@ -0,0 +1,4 @@
+// var: no reason to use it. It's a legacy feature.
+// let: use it when you need to reassign a variable. Allow it to reassign.
+// const: use it when you don't need to reassign a variable. not allow it to reassign.
+
