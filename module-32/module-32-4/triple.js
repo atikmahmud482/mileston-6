0a03dd96dfe96c97fd813a22ea19c62b7a4687ff
atikmahmud482 <atikmahmud482@gmail.com>
2024-12-29 00:12:04 +0600

    module-32-4

---
+++ module-32/module-32-4/triple.js
@@ -0,0 +1,14 @@
+/* 
+always us ===
+equal comparison doesn't work for non-primitive.
+*/
+
+const first = 3;
+const second = 3;
+
+if (first === second) {
+    console.log('is equal')
+}
+else {
+    console.log('not equal')
+}
