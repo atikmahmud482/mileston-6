8bb8bde0ff0e458802762218204db86d829ee089
atikmahmud482 <atikmahmud482@gmail.com>
2024-12-28 17:57:29 +0600

    module-32-1

---
+++ module-32/module-32-1/type.js
@@ -0,0 +1,16 @@
+//javascript primitive type
+const a = 5;
+const b = 'hi hello'
+const d = true;
+
+// no primitive type
+const ages = [45, 35, 49]
+const student = { id: 2, class: 5 }
+
+// console.log(typeof a, typeof b, typeof d, typeof ages, typeof student)
+
+let x = 5;
+let y = x
+console.log(x, y)
+y = 7;
+console.log(x, y)
