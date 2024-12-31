f5bdcb08ce865c1be003d9d059eed1f9f5624f62
atikmahmud482 <atikmahmud482@gmail.com>
2024-12-31 22:28:10 +0600

    module-32-5

---
+++ module-32/module-32-5/hoisting.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 5; i++) {
+    // console.log(i)
+}
+// console.log(i)
+print5()
+
+function print5() {
+    console.log(5)
+}
+
+var print10 = function () {
+    console.log(10)
+}
