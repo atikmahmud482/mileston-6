aa6626ab9cc2ed5134acf73ab6edf6feb7da1235
atikmahmud482 <atikmahmud482@gmail.com>
2024-12-31 23:35:24 +0600

    module-32-9

---
+++ module-32/module-32-9/summary.js
@@ -0,0 +1,12 @@
+/* 
+--primitive data type and non primitive
+--falsy value and trouty value
+--null and undefined
+--double equal == and triple equal ===
+--scope
+--hoisting , hoisting in function 
+--var vs const vs let(very important)
+--closure
+--callback function 
+--arguments and pass-by
+*/
