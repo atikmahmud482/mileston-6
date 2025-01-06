739fa71387693db8676d18660bae32afd8218fbc
atikmahmud482 <atikmahmud482@gmail.com>
2025-01-07 00:33:00 +0600

    module-34-4

--- module-34/phone.js
+++ module-34/phone.js
@@ -4,8 +4,7 @@ const loadPhone = async (searchText) => {
     const data = await res.json()
     // console.log(data.data);
     const phones = data.data;
-    displayPhones(phones)
-
+    displayPhones(phones);
 }
 const displayPhones = phones => {
     // console.log(phones)
@@ -14,6 +13,17 @@ const displayPhones = phones => {
     //clear phone container cards before adding new cards
     phoneContainer.textContent = '';
 
+    //display show all button of there are more than 12 phone
+    const showAllButton = document.getElementById('show-all-container')
+    if (phones.length > 12) {
+        showAllButton.classList.remove('hidden')
+    }
+    else {
+        showAllButton.classList.add('hidden')
+    }
+
+    //display only 12 phones
+    phones = phones.slice(0, 12)
 
     phones.forEach(phone => {
         console.log(phone);
@@ -48,10 +58,16 @@ const handelSearch = () => {
     // console.log('dfjjf')
     const searchFiend = document.getElementById('search-field');
     const searchText = searchFiend.value;
-    console.log(searchText);
+    // console.log(searchText);
     loadPhone(searchText)
 
 
+}
+
+const handelSearch2 = () => {
+    const searchField = document.getElementById('search-field2');
+    const searchText = searchField.value;
+    loadPhone(searchText)
 }
 
 // loadPhone()
